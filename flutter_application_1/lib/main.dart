5c62531d41ea01bf9143b446b759a12c3c189035
Chico27 <masamasa.987654321@gmail.com>
2021-11-16 19:15:07 +0900

    commit.

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('hey ninjas!'),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('my first app'),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Text(
+        'hello ninjas!',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+        ),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('click'),
+      backgroundColor: Colors.red[600],
+    ),
+  ),
 ));
